b4380bbd2b70fa12cdc4e1713934a6a27cb32323
Sarvar Abduqodirov <abduqodirovsarvar.2002@gmail.com>
2024-11-15 15:48:11 +0500

    change api address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://olmazor.api.olmatech.uz/api',
-  baseFileUrl: 'https://olmazor.api.olmatech.uz/api/File'
+  // baseUrl: 'https://olmazor.api.olmatech.uz/api',
+  // baseFileUrl: 'https://olmazor.api.olmatech.uz/api/File'
+  baseUrl: 'https://api.olmazor.uz/api',
+  baseFileUrl: 'https://api.olmazor.uz/api/File'
 };
